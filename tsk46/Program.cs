d8745244b20aecf7b2494c9b25141ae7c4d47e81
Artem <40538552+surge603@users.noreply.github.com>
2022-02-24 21:17:39 +0300

    Задача 46

---
+++ tsk46/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 46: Написать программу масштабирования фигуры
+
+int EnterNum()
+{
+    return int.Parse(Console.ReadLine());
+}
+
+Console.Clear();
+Console.WriteLine("Введите координаты вершин фигуры:");
+int[] x = new int[4];
+int[] y = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    Console.Write($"Введите х{i + 1}:");
+    x[i] = EnterNum();
+    Console.Write($"Введите y{i + 1}:");
+    y[i] = EnterNum();
+}
+Console.Write("Введите коэффициент масштабирования фигуры:");
+
+int k = EnterNum();
+
+Console.WriteLine("Координаты отмасштабированной фигуры:");
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine($"x{i + 1} = {x[i] * k}");
+    Console.WriteLine($"y{i + 1} = {y[i] * k}");
+}
