266c132bebe0954eba114237f0ca3921ecf79612
Artem <40538552+surge603@users.noreply.github.com>
2022-02-24 10:02:25 +0300

    Задача 42

--- tsk42/Program.cs
+++ tsk42/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 42: Определить, сколько чисел больше 0 введено с клавиатуры.
+
+Console.Clear();
+Console.WriteLine("Введите любое количество чисел. Ввод каждого числа завершайте клавишей Enter.");
+Console.WriteLine("Для завершения программы и подсчёта количества введенных отрицательных чисел введите \"-\"");
+int result = 0;
+while (true)
+{
+    Console.Write("Введите число или \"-\" для завершения: ");
+    string str = Console.ReadLine();
+    if (str == "-" || str == "")
+        break;
+    int num = int.Parse(str);
+    if (num < 0)
+    {
+        result++;
+        Console.WriteLine($"Введено отрицательное число!");
+    }
+}
+Console.WriteLine($"Введено отрицательных чисел: {result}");
